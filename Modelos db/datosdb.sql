b5dfe22634dac512836ad2c9d7ae29ac34a66290
jaredait <jaredampudia@gmail.com>
2022-03-30 12:00:15 -0500

    Agregar archivo para cargar datos a la db

---
+++ Modelos db/datosdb.sql
@@ -0,0 +1,12 @@
+/* INSERTAR CATEGORIAS */
+
+INSERT INTO CATEGORIA VALUES('CAMREG', 'Camiseta', 'Camisetas regulares cuello redondo', null, null);
+INSERT INTO CATEGORIA VALUES('CAMPOL', 'Polo', 'Camisetas polo', null, null);
+INSERT INTO CATEGORIA VALUES('PANTJEAN', 'Jean', 'Pantalones Jeans - Vaqueros', null, null);
+INSERT INTO CATEGORIA VALUES('PANTCHI', 'Chino', 'Pantalones Chinos', null, null);
+INSERT INTO CATEGORIA VALUES('PANTCARG', 'Cargo', 'Pantalones cargo', null, null);
+INSERT INTO CATEGORIA VALUES('PANTVEST', 'Vestir', 'Pantalones de vestir', null, null);
+INSERT INTO CATEGORIA VALUES('ZAPTEN', 'Tennis', 'Tennis', null, null);
+INSERT INTO CATEGORIA VALUES('ZAPDEP', 'Deportivo', 'Zapatos deportivos', null, null);
+INSERT INTO CATEGORIA VALUES('MED', 'Medias', 'Medias', null, null);
+INSERT INTO CATEGORIA VALUES('GAF', 'Lentes de Sol', 'Lentes de sol', null, null);
