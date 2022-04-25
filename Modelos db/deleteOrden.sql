bc8d507afa63388d69458c6a75f8b8b8a72f0d2b
jaredait <jaredampudia@gmail.com>
2022-04-25 18:02:26 -0500

    Agregar archivos para eliminar datos de la db

---
+++ Modelos db/deleteOrden.sql
@@ -0,0 +1,11 @@
+/* ORDEN PARA BORRAR LAS TABLAS */ 
+
+delete from DETALLE_FACTURA;
+delete from CATEGORIA_PRODUCTO;
+delete from FACTURA;
+delete from PARAMETROS_GENERALES;
+delete from CLIENTE;
+delete from CATEGORIA;
+delete from ENVIO;
+delete from PRODUCTO;
+delete from TIPO_PAGO;
---
+++ Modelos db/dropOrden.sql
@@ -0,0 +1,11 @@
+/* ORDEN PARA ELIMINAR LAS TABLAS */ 
+
+drop table DETALLE_FACTURA;
+drop table CATEGORIA_PRODUCTO;
+drop table FACTURA;
+drop table PARAMETROS_GENERALES;
+drop table CLIENTE;
+drop table CATEGORIA;
+drop table ENVIO;
+drop table PRODUCTO;
+drop table TIPO_PAGO;
